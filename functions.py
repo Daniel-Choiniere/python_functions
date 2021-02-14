6bc2da00efb091f38b7cd601f581ae97e2a2bc69
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-14 12:55:45 -0500

    instead of resuing the same code to check for a plaindrome, we can just call the ispalindrome fucntion

--- functions.py
+++ functions.py
@@ -3,18 +3,18 @@ def multiply(x, y):
     return result
 
 
-# def is_palindrome(string):
-#     # backwards = string[::-1]
-#     # return backwards == string
-#     return string[::-1].lower() == string.lower()
+def is_palindrome(string):
+    # backwards = string[::-1]
+    # return backwards == string
+    return string[::-1].lower() == string.lower()
 
 
-def is_sentence_palindrome(string):
+def is_sentence_palindrome(sentence):
     string = ""
     for char in sentence:
         if char.isalnum():
             string += char
-    return string[::-1].casefold() == string.casefold()
+    return is_palindrome(string)
 
 # my fist solution, not very efficent and not dynamic for a ll characters
 # def is_sentence_palindrome(string):
@@ -22,17 +22,11 @@ def is_sentence_palindrome(string):
 #            == string.replace(" ", "").replace("?", "").replace(",", "").lower()
 
 
-sentence = input("Please enter a sentence to check: ")
-if is_sentence_palindrome(sentence):
-    print("'{}' is a palindrome".format(sentence))
+word = input("Please enter a sentence to check: ")
+if is_sentence_palindrome(word):
+    print("'{}' is a palindrome".format(word))
 else:
-    print("'{}' is not a palindrome".format(sentence))
-
-# word = input("Please enter a word to check: ")
-# if is_palindrome(word):
-#     print("'{}' is a palindrome".format(word))
-# else:
-#     print("'{}' is not a palindrome".format(word))
+    print("'{}' is not a palindrome".format(word))
 
 
 # # two blank lines after a function declaration
