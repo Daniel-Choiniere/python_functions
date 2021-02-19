5fedc6dcc3511b2ded3d977d2b76fbfad3f0a3f2
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2021-02-19 09:21:18 -0500

    wrote a function for figuring out the fibonacci number of a given number

---
+++ fibonacci.py
@@ -0,0 +1,18 @@
+def fibonacci(n):
+    """ Return the `n` th fibonacci number, for positive `n` """
+    if 0 <= n <= 1:
+        return n
+
+    n1, n2 = 1, 0
+
+    result = None
+    for f in range(n - 1):
+        result = n1 + n2
+        n2 = n1
+        n1 = result
+
+    return result
+
+
+for i in range(36):
+    print(i, fibonacci(i))
